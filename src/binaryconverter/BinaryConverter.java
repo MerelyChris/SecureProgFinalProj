6a946410147e8e87042d14d7dbedbb221c5aed8b
Chris Burke <48271989+MerelyChris@users.noreply.github.com>
2022-12-07 01:21:48 -0600

    creating validInt method to simplify handInt

--- src/binaryconverter/BinaryConverter.java
+++ src/binaryconverter/BinaryConverter.java
@@ -1,5 +1,6 @@
 package binaryconverter;
 import java.util.Scanner;
+import java.math.BigDecimal;
 
 public class BinaryConverter {
 
@@ -7,7 +8,6 @@ public class BinaryConverter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
 
         String ans = "1";
         while(!ans.equals("0")){
@@ -31,11 +31,21 @@ public class BinaryConverter {
         } 
     }
     
-    public static boolean validAns(String ans){
-        if(ans.matches("^[0-2]?")){
+    public static boolean validAns(String inp){
+        if(inp.matches("^[0-2]?")){
             return true;
         }
         return false;
+    }
+
+    public static boolean validInt(String inp){
+        BigDecimal maxInt = new BigDecimal(2147483647);
+        BigDecimal bigDec = new BigDecimal(inp);
+        int res = maxInt.compareTo(bigDec);
+        if(!inp.matches("^[0-9]+$") || res == -1){
+            return false;
+        }
+        return true;
     }
 
     public static void handleBinary(Scanner input){
@@ -48,15 +58,14 @@ public class BinaryConverter {
         int decimal = Integer.parseInt(testBin,2);
         System.out.println(testBin + " as an integer is " + decimal);
     }
-    
+
     public static void handleInteger(Scanner input){
         System.out.print("Enter integer: ");
         String testInt = input.nextLine();
-        while(!testInt.matches("^[0-9]+$") || Integer.valueOf(testInt)>2147483647){
+        while(validInt(testInt) != true){
             System.out.print("Incorrect input, try again: ");
             testInt = input.nextLine();
         }
-        
         Integer finInt = Integer.parseInt(testInt);
         String binary = Integer.toBinaryString(finInt);
         System.out.println(testInt + " as an integer is " + binary);
